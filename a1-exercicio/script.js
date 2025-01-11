cac333075e4492d018ea58e3f81be2ce2c9db98b
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-01-11 11:49:55 -0300

    Pasta estilo adicionada

---
+++ a1-exercicio/script.js
@@ -0,0 +1,21 @@
+let currentInput = '';
+
+function press(value) {
+    currentInput += value;
+    document.getElementById('display').value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        document.getElementById('display').value = currentInput;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+        currentInput = '';
+    }
+}
